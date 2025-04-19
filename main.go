baf3dcf46067e2c0c6e1507ed316515a3c40d657
ucrnsemin <ucarenesemin@gmail.com>
2025-04-19 21:13:19 +0300

    Veritabanı bağlantısı güncellendi ve araç modeli eklendi

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/joho/godotenv"
 	"github.com/ucrnsemin/EnesGoland/config"
+	"github.com/ucrnsemin/EnesGoland/models"
 	"log"
 )
 
@@ -13,6 +14,37 @@ func main() {
 		log.Fatal("Error loading .env file", err.Error())
 		return
 	}
-	config.InitDB()
+	err = config.InitDB()
+	if err != nil {
+		return
+	}
+	// TABLO DÜZENLEMEK İÇİN AÇ
+	//err = config.DB.AutoMigrate(models.Vehicle{}, models.Driver{})
+	//if err != nil {
+	//	return
+	//}
+	var vehicle = models.Vehicle{
+		Name:            "Model K Perfomans Dört Çeker",
+		Brand:           "Tesla",
+		Model:           "Model K",
+		Version:         "2025",
+		MaxSpeed:        "280",
+		BatteryCapacity: "81",
+		WLTPRange:       "514",
+		RealRange:       "450",
+	}
+	// config.DB.Create(&vehicle)
+	//	config.DB.Delete(&vehicle)
+	vehicle = models.Vehicle{}
+	// config.DB.First(&vehicle, 2)
+	// config.DB.Delete(&vehicle, 1)
+	config.DB.First(&vehicle, 11)
+	vehicle.Version = "2000"
+	vehicle.MaxSpeed = "200"
+	config.DB.Save(&vehicle)
+
+	// 	TODO : driver çalış
+
+	// TODO vehicle dirver tablosu oluştur ve araç ile sürücü eşleştiren ablo oluşur
 
 }
