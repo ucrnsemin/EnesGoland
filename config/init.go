c42289070b78b5410564e965b122edf69cc227ae
ucrnsemin <ucarenesemin@gmail.com>
2025-05-11 15:37:39 +0300

    Add TimeZone configuration to database connection string in InitDB

--- config/init.go
+++ config/init.go
@@ -25,12 +25,14 @@ var DB *gorm.DB
 
 func InitDB() (err error) {
 
-	cnnString := fmt.Sprintf("host=%s user=%s password='%s' dbname=%s port=%s sslmode=disable",
+	cnnString := fmt.Sprintf("host=%s user=%s password='%s' dbname=%s port=%s sslmode=disable TimeZone=Europe/Istanbul",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"))
+
+	fmt.Println("Connecting to", cnnString)
 
 	DB, err = gorm.Open(postgres.Open(cnnString), &gorm.Config{})
 	if err != nil {
